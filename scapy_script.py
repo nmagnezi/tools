c6bf1ef3ae6ade916a9abb57672deb9584a014ac
nmagnezi <nmagnezi@redhat.com>
2015-02-19 14:52:43 +0200

    scapy script

---
+++ scapy_script.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+from scapy.all import *
+
+p = (Ether(src="f1:23:45:67:89:00", dst="ff:ff:ff:ff:ff:ff")
+     /IP(src="169.254.192.1", dst="224.0.0.18"))
+sendp(p, iface="br-int")

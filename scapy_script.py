7ea89949ba14817d4336bc13344c73a1cdc8eb98
nmagnezi <nmagnezi@redhat.com>
2015-02-19 15:22:43 +0200

    interface name update to br-tun

--- scapy_script.py
+++ scapy_script.py
@@ -4,4 +4,4 @@ from scapy.all import *
 
 p = (Ether(src="f1:23:45:67:89:00", dst="ff:ff:ff:ff:ff:ff")
      /IP(src="169.254.192.1", dst="224.0.0.18"))
-sendp(p, iface="br-int")
+sendp(p, iface="br-tun")

8d62bc08fba5ee9e70bd5fa1df71ef641526c1bb
nmagnezi <nmagnezi@redhat.com>
2015-04-21 15:19:45 +0300

    devstack tempest jenkins shell

---
+++ devstack-tempest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Jenkins Job
+##yes | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Copy key
+sshpass -p ${password} ssh-copy-id -i ~/.ssh/id_rsa.pub ${username}@${ip_address}
+
+# Get tox command
+if [ -z "$tests" ]; then
+   cmd=$(python scripts/gen_tempest_runner.py --directory ${directory})
+else
+   cmd=$(python scripts/gen_tempest_runner.py --tests ${tests} --directory ${directory})
+fi
+
+
+# Put tox command into a script
+chmod +x tempest.sh
+
+# Copy runner script
+scp -p tempest.sh ${username}@${ip_address}:${directory}/
+
+ssh root@${ip_address} "cd /opt/openstack/tempest && ${directory}/tempest.sh"
+
+
+# Collect results
+scp ${username}@${ip_address}:${directory}/tempest.log .
+scp ${username}@${ip_address}:${directory}/xunit.xml .

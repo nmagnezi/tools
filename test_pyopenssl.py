f0bbdedb6dc13b354806bf73e9509b14721c06d5
Nir Magnezi <nmagnezi@redhat.com>
2015-12-03 14:19:27 +0200

    Create test_pyopenssl.py

---
+++ test_pyopenssl.py
@@ -0,0 +1,35 @@
+import hashlib
+import hmac
+import requests
+from requests.auth import AuthBase
+import time
+
+
+class HMACAuth(AuthBase):
+
+    def __call__(self, request):
+        timestamp = str(int(time.time()))
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        secret = "TEST"
+
+        if not isinstance(message, bytes):
+            message = message.encode()
+        if not isinstance(secret, bytes):
+            secret = secret.encode()
+
+        signature = hmac.new(secret, message, hashlib.sha256).hexdigest()
+        request.headers.update({
+          'CB-VERSION': "",
+          'CB-ACCESS-KEY': "",
+          u'CB-ACCESS-SIGN': signature,
+          'CB-ACCESS-TIMESTAMP': timestamp,
+        })
+        return request
+
+#import ipdb ; ipdb.set_trace()
+session = requests.session()
+session.auth = HMACAuth()
+session.headers.update({'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'User-Agent': 'coinbase/python/2.0'})
+a = session.get("https://www.google.com")

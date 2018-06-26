c66d33b279250c8afcb1db90928a41d78b1a9211
Nir Magnezi <nmagnezi@redhat.com>
2018-06-26 15:45:30 +0300

    Delete devstack_sync.sh

--- devstack_sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-REMOTE_PATH="/opt/openstack/neutron/"
-LOCAL_PATH="/git/neutron"
-
-rsync --exclude=.tox --exclude=.idea --exclude=*.pyc --exclude=*.pyo --exclude=.venv -avzh --progress --delete root@$1:$REMOTE_PATH $LOCAL_PATH

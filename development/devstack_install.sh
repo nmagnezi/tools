0e43200345beb842729e64a961848849aef9e336
Nir Magnezi <nmagnezi@redhat.com>
2018-07-23 16:02:46 +0300

    Update devstack installer script to include more tools

--- development/devstack_install.sh
+++ development/devstack_install.sh
@@ -12,10 +12,12 @@ export SERVER_OS=$(awk -F "=" '/^NAME/ {print $2}' /etc/*-release)
 
 
 if [[ "$SERVER_OS" == '"CentOS Linux"' ]]; then
-    export USER=centos
+    export USER="centos"
+elif [[ "$SERVER_OS" == '"Red Hat Enterprise Linux Server"' ]]; then
+    export USER="cloud-user"
 else
     # "Ubuntu"
-    export USER=ubuntu
+    export USER="ubuntu"
 fi
 
 
@@ -23,15 +25,24 @@ fi
 useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 
-#
+# Copy the injected ssh keys to the stack user, for direct SSH access
 mkdir /opt/stack/.ssh
 cp /home/$USER/.ssh/authorized_keys /opt/stack/.ssh
 chown -R stack:stack /opt/stack/.ssh/
 chmod 700 /opt/stack/.ssh
 chmod 600 /opt/stack/.ssh/authorized_keys
-yum install -y vim git wget || apt-get install -y vim git wget
+
+# Install git and tools
+yum install -y vim git tig wget || apt-get install -y vim git tig wget
+
+# Clone my scripts
+sudo -u stack sudo git clone https://github.com/nmagnezi/scripts.git /opt/stack/devstack
+chown -R stack:stack /opt/stack/scripts
+
+
 sudo -u stack sudo git clone https://git.openstack.org/openstack-dev/devstack /opt/stack/devstack
 chown -R stack:stack /opt/stack/devstack
+
 
 echo ""
 echo "Now create your local.conf and stack"

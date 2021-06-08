2e8c25f5a2fdfd54a60ad3d88aba234449692695
Nir Magnezi <nmagnezi@redhat.com>
2021-06-08 19:18:20 +0300

    Create cluster_events_watcher.sh

---
+++ kubernetes/cluster_events_watcher.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ACI_NS=$1
+ACI=$2
+LINES=15
+
+eventsURL=$(kubectl -n $ACI_NS get agentclusterinstall $ACI -o=jsonpath="{.status.debugInfo.eventsURL}")
+watch -d "curl -k $eventsURL | jq "." | tail -${LINES}f"
+#echo $eventsURL

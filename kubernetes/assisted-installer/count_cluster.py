c71c932ef8c569a8fe2648896f85dd0cf3e6fa7c
Nir Magnezi <nmagnezi@redhat.com>
2022-09-20 10:02:44 +0300

    Improve count cluster logging

--- kubernetes/assisted-installer/count_cluster.py
+++ kubernetes/assisted-installer/count_cluster.py
@@ -1,18 +1,30 @@
 import json
-import pprint
+import logging
 import requests
 import subprocess
+import time
 
 CLUSTERS_FILE = './data/clusters.json'
 INFRAENV_FILE = './data/infraenvs.json'
 HOSTS_FILE = './data/hosts.json'
+HOSTS_GET_ATTEMPTS = 3
 DOWNLOAD_FILES = True
+LOG_DEBUG = False
+
+LOG = logging.getLogger(__name__)
+
 
 # prod
 URL = 'https://api.openshift.com/api/assisted-install/v2'
-# staging 
+# staging
 # URL = 'https://api.stage.openshift.com/api/assisted-install/v2'
 
+
+def configure_logger(debug_mode):
+    logging.basicConfig(
+        level=logging.DEBUG if debug_mode is True else logging.INFO,
+        format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s'
+    )
 
 def get_token():
     return subprocess.check_output('ocm token', shell=True).decode("utf-8")[:-1]
@@ -26,23 +38,24 @@ def load_json_from_file(file):
 
 def get_clusters(url, download_files):
     if download_files:
-        print("Downloading Clusters data...")
+        LOG.info("Downloading Clusters data...")
         clusters = requests.get(f'{url}/clusters', headers={'Authorization': f'Bearer {get_token()}'}).json()
-        print("Done!")
+        LOG.info("Done!")
         json_object = json.dumps(clusters, indent=4)
         with open(CLUSTERS_FILE, "w") as f:
             f.seek(0)
             f.write(json_object)
             f.truncate()
-    return load_json_from_file(CLUSTERS_FILE)
+    clusters = load_json_from_file(CLUSTERS_FILE)
+    return build_search_index_by_id(clusters)
 
 
 def get_infraenvs(url, download_files):
     if download_files:
-        print("Downloading InfraEnvs data...")
+        LOG.info("Downloading InfraEnvs data...")
         infraenvs = requests.get(f'{url}/infra-envs/',
                                  headers={'Authorization': f'Bearer {get_token()}'}).json()
-        print("Done!")
+        LOG.info("Done!")
         json_object = json.dumps(infraenvs, indent=4)
         with open(INFRAENV_FILE, "w") as f:
             f.seek(0)
@@ -54,17 +67,29 @@ def get_infraenvs(url, download_files):
 def get_hosts(url, download_files, infraenvs):
     infraenv_hosts = dict()
     if download_files:
-        print("Downloading Hosts data. This might take a while...")
+        LOG.info("Downloading Hosts data. This might take a while...")
         for i, infraenv in enumerate(infraenvs):
+            time.sleep(0.5)
             infraenv_id = infraenv["id"]
-            hosts = requests.get(f'{url}/infra-envs/{infraenv_id}/hosts',
-                                 headers={'Authorization': f'Bearer {get_token()}'}).json()
+            attempts = 3
+            hosts = []
+            for j in range(HOSTS_GET_ATTEMPTS):
+                try:
+                    hosts = requests.get(f'{url}/infra-envs/{infraenv_id}/hosts',
+                                         headers={'Authorization': f'Bearer {get_token()}'}).json()
+                except requests.exceptions.RequestException:
+                    LOG.debug(f"failed requesting hosts for infraenv {infraenv_id} - attempt ({j}/{attempts})")
+                    continue
+                break
             infraenv_hosts[infraenv['id']] = list()
+            if not hosts:
+                LOG.debug(f"adding infraenv ({i}/{len(infraenvs)}) {infraenv_id} has no hosts")
+                continue
             for host in hosts:
                 host_id = host['id']
                 infraenv_hosts[infraenv['id']].append(host)
-                print(f"DEBUG: adding infraenv ({i}/{len(infraenvs)}) {infraenv_id} host {host_id}")
-        print("Done!")
+                LOG.debug(f"adding infraenv ({i}/{len(infraenvs)}) {infraenv_id} host {host_id}")
+        LOG.info("Done!")
         json_object = json.dumps(infraenv_hosts, indent=4)
         with open(HOSTS_FILE, "w") as f:
             f.seek(0)
@@ -104,17 +129,19 @@ def compare_macs(infraenv, hosts):
 
 
 if __name__ == '__main__':
+    configure_logger(LOG_DEBUG)
+    cluster_states = {'no_status': 0}
     count_static_network_configs = 0
     count_unbound_infraenvs = 0
     cluster_not_found = 0
     infra_env_with_no_hosts = 0
-    cluster_states = {'no_status': 0}
+    covering_all_macs = 0
+
     clusters = get_clusters(URL, DOWNLOAD_FILES)
     infraenvs = get_infraenvs(URL, DOWNLOAD_FILES)
     hosts = get_hosts(URL, DOWNLOAD_FILES, infraenvs)
-    covering_all_macs = 0
-    clusters_search_index = build_search_index_by_id(clusters)  # better search times.
 
+    # Process infraenvs
     for infraenv in infraenvs:
         static_network_config = infraenv.get('static_network_config')
         # Do not process infraenvs with no static_network_config
@@ -127,7 +154,7 @@ if __name__ == '__main__':
         if not(cluster_id and cluster_id != ''):
             count_unbound_infraenvs += 1
         else:  # Bounded infraenvs
-            infraenv_cluster = clusters_search_index.get(cluster_id)
+            infraenv_cluster = clusters.get(cluster_id)
             if not infraenv_cluster:
                 cluster_not_found += 1
                 continue
@@ -135,6 +162,7 @@ if __name__ == '__main__':
             if not infraenv_cluster_status or infraenv_cluster_status == '':
                 cluster_states['no_status'] += 1
             else:
+                # Should not compare mac addresses for infraenvs with no hosts.
                 if not hosts.get(infraenv['id']):
                     infra_env_with_no_hosts += 1
                 else:
@@ -145,14 +173,14 @@ if __name__ == '__main__':
                     cluster_states[infraenv_cluster_status] = 1
 
     # Results
-    print(f"found {len(clusters)} clusters and {len(infraenvs)} infraenvs total")
-    print(f"found: {count_unbound_infraenvs} unbounded infraenvs")
-    print(f"found: {cluster_not_found} infraenvs that point to a cluster that cannot be found\n")
+    LOG.info(f"found {len(clusters)} clusters and {len(infraenvs)} infraenvs total")
+    LOG.info(f"found: {count_unbound_infraenvs} unbounded infraenvs")
+    LOG.info(f"found: {cluster_not_found} infraenvs that point to a cluster that cannot be found\n")
 
-    print(f"found {len(infraenvs)-count_static_network_configs} infraenvs without static_network_configs")
-    print(f"found {count_static_network_configs} infraenvs with static_network_configs. breakdown by status:")
-    pprint.pprint(cluster_states)
-    print(f"found that out of {count_static_network_configs} infraenvs with static_network_configs"
+    LOG.info(f"found {len(infraenvs)-count_static_network_configs} infraenvs without static_network_configs")
+    LOG.info(f"found {count_static_network_configs} infraenvs with static_network_configs. breakdown by status:")
+    LOG.info(cluster_states)
+    LOG.info(f"found that out of {count_static_network_configs} infraenvs with static_network_configs"
           f" --> {covering_all_macs} covering all macs ; {infra_env_with_no_hosts} infraenv with no hosts")
 
 
